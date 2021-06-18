76686104198cdd371836d79b718cb87c99b9f208
Maciej Lewicki <lewickimapl@gmail.com>
2021-06-19 00:21:26 +0200

    1: create basic chord diagram generator

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import reportWebVitals from './reportWebVitals';
+import Chord from './components/chord';
 
 const App = () => {
   return (
-    <Button variant='contained' color='primary'>
-      Hello world
-    </Button>
+    <>
+    <Chord 
+      name='A'
+      tuning={['E', 'B', 'G', 'D', 'A', 'E']}
+      notes={[-1, 0, 2, 2, 2, 0]} />
+
+    <Chord 
+      name='B'
+      tuning={['E', 'B', 'G', 'D', 'A', 'E']}
+      notes={[-1, 2, 4, 4, 4, 2]} />
+    </>
   );
 }
 
